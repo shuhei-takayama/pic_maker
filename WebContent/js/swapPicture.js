2b7738f2501e775b6d465736599078b99ee8c0ea
shuhei-takayama <takayama.shuhei@i.softbank.jp>
2014-12-11 14:51:30 +0900

    fix: swap picture

--- WebContent/js/swapPicture.js
+++ WebContent/js/swapPicture.js
@@ -1,7 +1,7 @@
-function swap(ctx, img, comp) {
+function swap(ctx, img, comp, canvas) {
 	var mainComp = comp[0];
 
-	for (var i = 0; i < comp.length; i++) {
+	for (var i = 1; i < comp.length; i++) {
 		var x, y, h, w;
 		x = comp[i].x;
 		y = comp[i].y;
@@ -14,13 +14,19 @@ function swap(ctx, img, comp) {
 		mw = mainComp.width;
 		mh = mainComp.height;
 
+		ctx.beginPath();
+		ctx.scale(1.4, 1.6);
+		ctx.arc(x+w/2, y+h/2, w/2, 0, Math.PI * 2, false);
+		ctx.clip();
+		/* 画像を指定 */
 		ctx.drawImage(img, mx, my, mw, mh, x, y, w, h);
+		console.log("gooood"+comp.length);
 	}
 }
 
-setOnLoadPicture(function(){
+
+setOnLoadPicture(function() {
 	var obj = getParameter();
 	swap(obj.ctx, obj.img, obj.comp);
 });
-
 
